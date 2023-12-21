89f87f52550a56c69972538f48cfd144ff8be512
saho-north <SahoKitahara@my.uopeople.edu>
2023-12-21 19:58:06 +0900

    added comment to error.c

--- srcs/error.c
+++ srcs/error.c
@@ -6,7 +6,7 @@
 /*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 19:49:04 by sakitaha          #+#    #+#             */
-/*   Updated: 2023/12/21 19:50:09 by sakitaha         ###   ########.fr       */
+/*   Updated: 2023/12/21 19:56:27 by sakitaha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,36 @@
 
 /**
  * error.c:
- * error handling (exit the program and display a message when called)
+ * This file contains functions for error handling in the FDF project.
+ *
+ * Functions in this file include:
+ * - perror_exit: Displays a system error message based on the current errno
+ *   and exits the program with EXIT_FAILURE status. This function is used
+ *   when a system call fails and we need to provide feedback about the error.
+ *
+ * - print_error: Writes a custom error message to the standard error output
+ *   (stderr). This function is used for displaying non-system errors that
+ *   require attention.
+ *
+ * - print_error_exit: Combines the functionality of print_error and then
+ *   exits the program. This is useful for handling errors where the program
+ *   cannot continue its execution and needs to terminate immediately after
+ *   displaying an error message.
+ *
+ * - free_point_matrix: Safely frees a dynamically allocated 2D array (matrix)
+ *   of t_point structures. This function is essential for preventing memory
+ *   leaks by ensuring that all allocated memory is properly freed when
+ *   an error occurs or when the program ends.
+ *
+ * - free_and_print_error_exit: Integrates the functionality of freeing the
+ *   2D array and then displaying an error message before exiting. This is a
+ *   higher-level function that simplifies error handling by combining
+ *   resource cleanup and error messaging in one call.
+ *
+ * Note: These functions are designed to provide a robust error handling
+ * mechanism throughout the FDF project, ensuring that resources are properly
+ * managed and errors are reported clearly, aiding in debugging and maintaining
+ * the program's integrity.
  */
 
 void	perror_exit(char *message)
