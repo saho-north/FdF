e08c3dd9b12f75ca1e99530938d9bdcea550c6b3
saho-north <SahoKitahara@my.uopeople.edu>
2024-02-11 00:59:57 +0900

    refactored rotation logic

--- srcs/rotation.c
+++ srcs/rotation.c
@@ -1,140 +1,75 @@
 
 #include "fdf.h"
 #include <math.h>
-#include <stdio.h>
 
-
-t_point	rotation_x(t_point points, double angle)
-{
-	t_point	rotated;
-	double	rad;
-
-	rad = angle * M_PI / 180.0;
-	rotated.x = points.x;
-	rotated.y = points.y * cos(rad) - points.z * sin(rad);
-	rotated.z = points.y * sin(rad) + points.z * cos(rad);
-	return (rotated);
-}
-
-t_point	rotation_y(t_point points, double angle)
-{
-	t_point	rotated;
-
-	double rad = angle * M_PI / 180.0; // 角度をラジアンに変換
-	rotated.x = points.x;
-	rotated.y = points.y * cos(rad) - points.z * sin(rad);
-	rotated.z = points.y * sin(rad) + points.z * cos(rad);
-	return (rotated);
-}
-
-t_point	rotation_x(t_point points, double angle)
-{
-	t_point	rotated;
-
-	double rad = angle * M_PI / 180.0; // 角度をラジアンに変換
-	rotated.x = points.x;
-	rotated.y = points.y * cos(rad) - points.z * sin(rad);
-	rotated.z = points.y * sin(rad) + points.z * cos(rad);
-	return (rotated);
-}
-
-int	main(void)
-{
-	t_point	point;
-	t_point	rotatedPoint;
-
-	point = {1, 2, 3};
-	rotatedPoint = rotation_x(point, 10);
-	printf("Rotated Point: x=%f, y=%f, z=%f\n", rotatedPoint.x, rotatedPoint.y,
-			rotatedPoint.z);
-	return (0);
-}
-
-Matrix4x4	createRotationMatrix(float angle){
-	Rotation matrix around the Z - axis}
-
-Matrix4x4	createTranslationMatrix(float dx, float dy, float dz){
-	Translation matrix}
-
-Matrix4x4	createScalingMatrix(float sx, float sy, float sz){
-	Scaling matrix}
-
-Matrix4x4			combinedTransformation = createScalingMatrix(sx, sy, sz)
-			* createRotationMatrix(angle) * createTranslationMatrix(dx, dy, dz);
-
-
-//----
-
-t_point	rotation_x(t_point points, double angle)
-{
-	t_point	rotated;
-
-	double rad = angle * M_PI / 180.0; // 角度をラジアンに変換
-	rotated.x = points.x;
-	rotated.y = points.y * cos(rad) - points.z * sin(rad);
-	rotated.z = points.y * sin(rad) + points.z * cos(rad);
-	return (rotated);
-}
-
-int	main(void)
-{
-	t_point	point;
-	t_point	rotatedPoint;
-
-	point = {1, 2, 3};
-	rotatedPoint = rotation_x(point, 10);
-	printf("Rotated Point: x=%f, y=%f, z=%f\n", rotatedPoint.x, rotatedPoint.y,
-			rotatedPoint.z);
-	return (0);
-}
-
-//x軸周りの回転
+/**
+ * Rotates the given point around the X-axis.
+ */
 void	rotate_x(t_point *point, float degrees)
 {
-	float	x;
-	float	y;
-	float	z;
+	float	original_y;
 	float	radians;
 
-	x = point->x;
-	y = point->y;
-	z = point->z;
+	original_y = point->y;
 	radians = degrees * M_PI / 180.0;
-	point->x = x;
-	point->y = y * cos(radians) - z * sin(radians);
-	point->z = y * sin(radians) + z * cos(radians);
+	point->y = original_y * cos(radians) - point->z * sin(radians);
+	point->z = original_y * sin(radians) + point->z * cos(radians);
 }
 
-//y軸周りの回転
+/**
+ * Rotates the given point around the Y-axis.
+ */
 void	rotate_y(t_point *point, float degrees)
 {
-	float	x;
-	float	y;
-	float	z;
+	float	original_x;
 	float	radians;
 
-	x = point->x;
-	y = point->y;
-	z = point->z;
+	original_x = point->x;
 	radians = degrees * M_PI / 180.0;
-	point->x = x * cos(radians) + z * sin(radians);
-	point->y = y;
-	point->z = -x * sin(radians) + z * cos(radians);
+	point->x = original_x * cos(radians) + point->z * sin(radians);
+	point->z = -original_x * sin(radians) + point->z * cos(radians);
 }
 
-//z軸周りの回転
+/**
+ * Rotates the given point around the Z-axis.
+ */
 void	rotate_z(t_point *point, float degrees)
 {
-	float	x;
-	float	y;
-	float	z;
+	float	original_x;
+	float	original_y;
 	float	radians;
 
-	x = point->x;
-	y = point->y;
-	z = point->z;
+	original_x = point->x;
+	original_y = point->y;
 	radians = degrees * M_PI / 180.0;
-	point->x = x * cos(radians) - y * sin(radians);
-	point->y = x * sin(radians) + y * cos(radians);
-	point->z = z;
+	point->x = original_x * cos(radians) - original_y * sin(radians);
+	point->y = original_x * sin(radians) + original_y * cos(radians);
+}
+
+/**
+ * Rotates the map around the X, Y and Z axes.
+*/
+void	rotattion(t_fdf *fdf)
+{
+	size_t	x;
+	size_t	y;
+
+	y = 0;
+	while (y < fdf->max_y)
+	{
+		x = 0;
+		while (x < fdf->max_x)
+		{
+			if (fdf->points[y][x].is_exist == false)
+			{
+				break ;
+			}
+			//TODO: ZOOMは別の関数でやるのか？まだよくわからないです。何回もiterateすることになるから無駄になりそうだけど、、
+			rotate_x(&fdf->points[y][x], fdf->ctrl.x_degree);
+			rotate_y(&fdf->points[y][x], fdf->ctrl.y_degree);
+			rotate_z(&fdf->points[y][x], fdf->ctrl.z_degree);
+			x++;
+		}
+		y++;
+	}
 }
